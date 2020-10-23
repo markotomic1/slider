dc99eda30e3e703c0c545aa968338cce8ecba06c
Marko <tomicmark3@gmail.com>
2020-10-23 18:47:06 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  useState,
+  useEffect,
+} from "react";
+import data from "./data";
+import {
+  FiChevronRight,
+  FiChevronLeft,
+} from "react-icons/fi";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [people, setPeople] = useState(data);
+
+  useEffect(() => {
+    if (value < 0) {
+      setValue(people.length - 1);
+    } else if (value > people.length - 1) {
+      setValue(0);
+    }
+  }, [people, value]);
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setValue(value + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [value]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='slider-section'>
+        <div className='title'>
+          <h2>Slider</h2>
+          <div className='underline'></div>
+        </div>
+
+        {people.map((item, index) => {
+          if (index === value) {
+            return (
+              <div className='card'>
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className='img'
+                />
+
+                <div className='heading'>
+                  <button
+                    onClick={() =>
+                      setValue(value - 1)
+                    }
+                    className='minus'
+                  >
+                    <FiChevronLeft />
+                  </button>
+                  <div className='heading-text'>
+                    <h2>{item.name}</h2>
+                    <p>{item.title}</p>
+                  </div>
+                  <button
+                    onClick={() =>
+                      setValue(value + 1)
+                    }
+                    className='plus'
+                  >
+                    <FiChevronRight />
+                  </button>
+                </div>
+                <p className='para'>
+                  {item.quote}
+                </p>
+              </div>
+            );
+          }
+        })}
+      </section>
+    </main>
   );
 }
 
